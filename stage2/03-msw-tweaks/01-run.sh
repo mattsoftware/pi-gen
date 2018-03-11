d2a1888b2b8bbe86e3065e3ccff7b42853febcc9
Matt Paine <matt@mattsoftware.com>
2018-03-11 21:34:35 +1000

    Add some tweaks for installing amazon ssm agent, provisioning ssm from /boot/ssm, add some extra packages and the msw_provision scripts (/boot/msw_provision.sh)

---
+++ stage2/03-msw-tweaks/01-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+install -m 644 files/msw_provision.service ${ROOTFS_DIR}/etc/systemd/system/msw_provision.service
+install -m 644 files/msw_provisioned.service ${ROOTFS_DIR}/etc/systemd/system/msw_provisioned.service
+install -m 644 files/ssmsetup.service ${ROOTFS_DIR}/etc/systemd/system/ssmsetup.service
+
+on_chroot << EOF
+curl https://s3-ap-southeast-2.amazonaws.com/amazon-ssm-ap-southeast-2/latest/debian_arm/amazon-ssm-agent.deb -o /root/amazon-ssm-agent.deb
+dpkg -i /root/amazon-ssm-agent.deb
+rm /root/amazon-ssm-agent.deb
+systemctl disable amazon-ssm-agent
+systemctl enable msw_provision
+systemctl enable msw_provisioned
+systemctl enable ssmsetup
+EOF
+
