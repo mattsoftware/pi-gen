21237a6f9de7d51c57c8ed92939a483c43e289e7
Matt Paine <matt@mattsoftware.com>
2018-03-12 14:28:15 +1000

    Add amazon ssm agent and the ability to provision on boot through /boot/ssm

--- stage2/03-msw-tweaks/01-run.sh
+++ stage2/03-msw-tweaks/01-run.sh
@@ -2,9 +2,14 @@
 
 install -m 644 files/msw_provision.service ${ROOTFS_DIR}/etc/systemd/system/msw_provision.service
 install -m 644 files/msw_provisioned.service ${ROOTFS_DIR}/etc/systemd/system/msw_provisioned.service
+install -m 644 files/ssmsetup.service ${ROOTFS_DIR}/etc/systemd/system/ssmsetup.service
 
 on_chroot << EOF
+curl https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_arm/amazon-ssm-agent.deb -o /root/amazon-ssm-agent.deb
+dpkg -i /root/amazon-ssm-agent.deb
+systemctl disable amazon-ssm-agent
 systemctl enable msw_provision
 systemctl enable msw_provisioned
+systemctl enable ssmsetup
 EOF
 
