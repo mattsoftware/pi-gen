85e949cbb99c76dafa7b8a0a2337e78ebeecd2d8
Matt Paine <matt@mattsoftware.com>
2018-04-19 00:54:44 +1000

    Add a service to test for network connectivity before starting the network-online.target

--- stage2/03-msw-tweaks/01-run.sh
+++ stage2/03-msw-tweaks/01-run.sh
@@ -4,6 +4,7 @@ install -m 644 files/journald.conf ${ROOTFS_DIR}/etc/systemd/journald.conf
 install -m 644 files/msw_provision.service ${ROOTFS_DIR}/etc/systemd/system/msw_provision.service
 install -m 644 files/msw_provisioned.service ${ROOTFS_DIR}/etc/systemd/system/msw_provisioned.service
 install -m 644 files/ssmsetup.service ${ROOTFS_DIR}/etc/systemd/system/ssmsetup.service
+install -m 644 files/network-wait-online.service ${ROOTFS_DIR}/etc/systemd/system/network-wait-online.service
 install -m 644 files/editor.sh ${ROOTFS_DIR}/etc/profile.d/editor.sh
 
 on_chroot << EOF
@@ -12,6 +13,7 @@ dpkg -i /root/amazon-ssm-agent.deb
 systemctl enable msw_provision
 systemctl enable msw_provisioned
 systemctl enable ssmsetup
+systemctl enable network-wait-online
 
 # amazon-ssm-agent struggles with running ansible without this fix
 sed -i.bak 's|^#*remote_tmp[^=]*=.*$|remote_tmp = /tmp/ansible|' /etc/ansible/ansible.cfg
