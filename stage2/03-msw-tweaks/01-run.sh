2892ac8d60d736fcc02c3806173ef179d07fc7de
Matt Paine <matt@mattsoftware.com>
2018-04-19 00:51:55 +1000

    Set the default editor to be vim

--- stage2/03-msw-tweaks/01-run.sh
+++ stage2/03-msw-tweaks/01-run.sh
@@ -3,6 +3,7 @@
 install -m 644 files/msw_provision.service ${ROOTFS_DIR}/etc/systemd/system/msw_provision.service
 install -m 644 files/msw_provisioned.service ${ROOTFS_DIR}/etc/systemd/system/msw_provisioned.service
 install -m 644 files/ssmsetup.service ${ROOTFS_DIR}/etc/systemd/system/ssmsetup.service
+install -m 644 files/editor.sh ${ROOTFS_DIR}/etc/profile.d/editor.sh
 
 on_chroot << EOF
 curl https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_arm/amazon-ssm-agent.deb -o /root/amazon-ssm-agent.deb
